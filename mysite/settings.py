567f1a800fc84ef7bbbf7dba97752932cbded0ca
KM Yoo <countmonte7@naver.com>
2018-12-16 22:49:00 +0900

    module import error fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -113,5 +112,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
